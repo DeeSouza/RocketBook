f223f738c6eb68c5055519b72420e15ca96cc586
Diego Souza <dee-souza@hotmail.com>
2019-06-12 12:00:29 -0300

    Feat: Add dependency to async await babel

--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ import Post from "./components/Post";
 
 import "./assets/style.scss";
 
+import API from "./config/api";
+
 class App extends Component {
+    state = {
+        posts: []
+    };
+
+    async componentDidMount() {
+        const posts = await API.get("posts");
+        console.log(posts);
+    }
+
     render() {
         return (
             <div id="wrapper">
